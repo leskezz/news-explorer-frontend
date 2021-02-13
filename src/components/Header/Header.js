7365940e6cf75c54021c214326a5fa467377523f
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2021-02-13 17:56:28 +0300

    Добавь микроанимацию всех кнопок

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
 import SearchForm from '../SearchForm/SearchForm';
@@ -6,7 +7,7 @@ function Header({ blackTheme }) {
     return(
         <header className={`header ${blackTheme ? 'header_black' : 'header_white'}`}>
             <div className={`header__top ${blackTheme ? 'header__top_black' : 'header__top_white'}`}>
-                <p className={`header__logo ${blackTheme ? 'header__logo_black' : 'header__logo_white'}`}>NewsExplorer</p>
+                <Link exact to="/" className={`header__logo ${blackTheme ? 'header__logo_black' : 'header__logo_white'}`}>NewsExplorer</Link>
                 <Navigation blackTheme={blackTheme} />
             </div>
             {
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,13 @@
+import {useState} from 'react';
 import './SearchForm.css';
 
 function SearchForm() {
+    const [isSearchClicked, setIsSearchClicked] = useState(false);
+
+    function handleClickSearch() {
+        setIsSearchClicked(!isSearchClicked);
+    }
+
     return(
         <form className="search-form" name="search">
             <h1 className="search-form__title">
@@ -19,7 +26,7 @@ function SearchForm() {
                 maxLength="40"
                 required
                 ></input>
-                <button type="submit" className="search-form__submit">Искать</button>
+                <button onMouseUp={handleClickSearch} onMouseDown={handleClickSearch} type="submit" className={`search-form__submit ${isSearchClicked && 'search-form__submit_clicked'}`}>Искать</button>
             </fieldset>
         </form>
     )
